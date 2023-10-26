77158849291013c169e0e17bd1b7c9799fdcb0ea
Igor Borges <igor.borges@grupoplantao.srv.br>
2023-10-26 09:14:12 -0300

    :rocket: deploy sistema

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -2,17 +2,26 @@ import Banner from 'components/Banner'
 import styles from './player.module.css'
 import Titulo from 'components/Titulo'
 import { useParams } from 'react-router-dom'
-import videos from 'json/db.json'
 import NaoEncontrada from 'components/NaoEncontrada'
+import { useEffect, useState } from 'react'
 
 
 export default function Player() {
 
-    
+    const [video, setVideo] = useState();
     const parametros = useParams();
-    const video = videos.find((video) => {
-        return video.id === Number(parametros.id);
-    });
+
+    useEffect(() => {
+        fetch(`https://my-json-server.typicode.com/monicahillman/cinetag-api/videos?id=${parametros.id}`)
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideo(...dados)
+            })
+    }, []);
+
+    // const video = videos.find((video) => {
+    //     return video.id === Number(parametros.id);
+    // });
 
     if (!video) {
         return <NaoEncontrada />
